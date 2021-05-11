b4dbe64ca086057fe165de4398634c9e88bb74a5
renan-auer <renan.auer@gmail.com>
2021-05-11 20:25:37 -0300

    feat: dashboard screen

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +12,63 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.setSalesGraph()
+    this.setClientGraph()
+  }
+
+  setSalesGraph() {
+    var ctxVendas : any = document.getElementById('chartVendas');
+    var myChartVendas = new Chart(ctxVendas.getContext('2d'), {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  setClientGraph() {
+    var ctxClient : any = document.getElementById('chartClientes');
+    var myChartClient = new Chart(ctxClient.getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      }
+    });
   }
 
 }
