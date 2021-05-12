e4457ee082f4f93b12b706d38f29eda0333eac6e
renan-auer <renan.auer@gmail.com>
2021-05-11 23:07:56 -0300

    feat: define routes

--- server/src/config/routes.ts
+++ server/src/config/routes.ts
@@ -1,7 +1,11 @@
 import { UserRoutes } from '@routes/UserRoute';
+import { DashboardRoute } from '@routes/DashboardRoute';
+import { LoginRoute } from '@routes/LoginRoute';
 
 const API = '/api';
 
 export default (app) => {
   app.use(API, UserRoutes);
+  app.use(API, LoginRoute);
+  app.use(API, DashboardRoute);
 };
