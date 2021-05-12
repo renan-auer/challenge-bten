0f7e150f0d1886f27b7db27b5b584ecb2e25959f
renan-auer <renan.auer@gmail.com>
2021-05-11 23:09:50 -0300

    feat: signin requests to api

--- client/src/app/pages/signin/signin.component.ts
+++ client/src/app/pages/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user-service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
-
+  formulario: FormGroup;
+  isSubmitted: boolean = false;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) { 
+    this.formulario = this.formBuilder.group({})
+  }
+  
   ngOnInit(): void {
+    this.setForm()
   }
 
+  setForm() {
+    this.isSubmitted = false;
+    this.formulario = this.formBuilder.group({
+      name: [null, Validators.required],
+      password: [null, Validators.required],
+      confPassword: [null, Validators.required],
+      height: [null, Validators.required],
+      age: [null, Validators.required],
+    })
+  }
+
+  save(user: any) {
+    this.isSubmitted = true
+    if (this.formulario.invalid)
+      return
+
+      this.userService.save(user).subscribe(data => {
+      this.router.navigate(['/login'])
+    })
+  }
+
+  get f() { return this.formulario.controls }
 }
